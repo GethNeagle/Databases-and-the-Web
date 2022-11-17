a52b97b70c8556523624d60b446f1a05efda131d
GethNeagle <103122506+GethNeagle@users.noreply.github.com>
2022-11-17 11:46:28 +0000

    Tidies up code. Changed Redirect to 'about' from 'registered' page to prevent error sending the user to golds home page.

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const e = require('express');
 
+//required for validation
 const { check, validationResult }
     = require('express-validator');
 
@@ -31,55 +32,55 @@ module.exports = function(app, shopData) {
             if (err) {
                 res.redirect('./'); 
             }
+            //if no errors
             let newData = Object.assign({}, shopData, {availableBooks:result});
             console.log(newData)
             res.render('list.ejs', newData)
          });        
     });
+
+    //gets the register page
     app.get('/register', function (req,res) {
         res.render('register.ejs', shopData);                                                                     
     });     
     
-    
+    //posts the registered page
     app.post('/registered',[
-        check('username', 'Username should be more than 4 characters')
+        check('username', 'Username should be more than 4 characters')//checks username is longer than 4 chars
                     .isLength({ min: 4, max: 30 }),
-        check('first', 'First name length should be more than')
+        check('first', 'First name length should be more than')//checks first name is longer than 2 chars
                     .isLength({ min: 2, max: 50 }),
         check('last', 'Last name should be more than 2 characters')
                     .isLength({ min: 2, max: 50 }),
-        check('email', 'Email should be more than 8 characters')
-                    .isEmail().isLength({ min: 8, max: 30 }),
-        check('password', 'Password length should be 8 to 10 characters')
+        check('email', 'Email should be more than 8 characters')//checks email is an email
+                    .isEmail().isLength({ min: 8, max: 50 }),
+        check('password', 'Password length should be 8 to 10 characters')//checks password is at least 8 chars long
                     .isLength({ min: 8, max: 10 })
     ], function (req,res) {
         //initialise bcrypt
         const bcrypt = require('bcryptjs');
         const saltRounds = 10;
         const plainPassword = req.body.password;
-        
-          //hashing the password
+        //hashing the password
         bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) { 
             console.log(hashedPassword);
             let sqlquery = "INSERT INTO users (username, first, last, email, hashedPassword) VALUES (?,?,?,?,?)";
             let newrecord = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword];
-            const errors = validationResult(req);
+            const errors = validationResult(req);//validation happens here
             if (!errors.isEmpty()){
-                res.json(errors)
+                res.json(errors)//prints any errors with validation the to screen
             }
             else{
             db.query(sqlquery, newrecord, (err, result) => {
-             if (err) {
-               return console.error(err.message);
-             }
-             else
-             result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered! We will send an email to you at ' + req.body.email;
-             result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
-             res.send(result);
-             
+                if (err) {
+                    return console.error(err.message);
+                }
+                else
+                result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered! We will send an email to you at ' + req.body.email;
+                result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
+                res.send(result);
             })
-            
-    }})   
+        }})
     });
 
 
@@ -94,7 +95,7 @@ module.exports = function(app, shopData) {
             let newData = Object.assign({}, shopData, {availableBooks:result});
             console.log(newData)
             res.render('list.ejs', newData)
-         });
+        });
     });
 
 
@@ -103,13 +104,12 @@ module.exports = function(app, shopData) {
     app.get('/listusers', function(req, res) {
         // If the user is loggedin
         if (req.session.loggedin) {
-            console.log("here now")
             let sqlquery = "SELECT * FROM users"; // query database to get all the books
             db.query(sqlquery, (err, result) => {
                 let newData = Object.assign({}, shopData, {availableUsers:result});
-                res.render('listusers.ejs', newData);  
-             });
-             return;
+                res.render('listusers.ejs', newData)
+            });
+            return;
             } else {
             // Not logged in
             res.send('Please login to view this page!');
@@ -120,18 +120,12 @@ module.exports = function(app, shopData) {
     app.get('/listusers', function(req, res) {
         // If the user is loggedin
         if (req.session.loggedin) {
-            console.log("here now") // Output username
             let sqlquery = "SELECT * FROM users"; // query database to get all the books
             // execute sql query
-           
             db.query(sqlquery, (err, result) => {
-                
-  
                 let newData = Object.assign({}, shopData, {availableUsers:result});
                 res.render('listusers.ejs', newData);
-                
-             });
-             
+            });
         } else {
             // Not logged in
             res.send('Please login to view this page!');
@@ -139,22 +133,21 @@ module.exports = function(app, shopData) {
         res.end();
     });
 
+    //gets logout page    
     app.get('/logout', function (req, res) {
         res.render('logout.ejs', shopData);
      });
 
+     //posts logges out - will post an error message if no user is logged in
      app.post('/loggedout', async (req, res) => {
         if (req.session.loggedin) {
             delete req.session.loggedin;
             res.send({result: 'SUCCESS'});
         } else {
+            //if no user logged in, send error message
             res.json({result: 'ERROR', message: 'User is not logged in.'});
         }
     });
-
-    
-
-
 
     app.get('/login', function (req, res) {
         res.render('login.ejs', shopData);
@@ -179,31 +172,28 @@ module.exports = function(app, shopData) {
                     res.send("Password does not match");
                 }
                 else if (result == true) {
-                    
+                    //sets variabel to true, will allow access to otherwise limited pages      
                     req.session.loggedin = true;
-                    console.log(req.session.loggedin);
+                    console.log(req.session.loggedin);//to print to console that 'login' worked
 				    req.session.username = username;
-                    res.redirect('/');
+                    res.redirect('about');//sends users back to index
                 }
                 else {
                     res.send("Your Username or Password are incorrect");
                 }
             });
-              
-          });
-
+        });
     });
 
 
 
 
-
+    //renders addbook page
     app.get('/addbook', function (req, res) {
         if (req.session.loggedin){
-
-        
-        res.render('addbook.ejs', shopData);
+            res.render('addbook.ejs', shopData);
         }else{
+            //if no users logged in, will prompt user to log in
             res.send("Please log in");
         }
      });
@@ -213,34 +203,22 @@ module.exports = function(app, shopData) {
            let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
            // execute sql query
            let newrecord = [req.body.name, req.body.price];
+           //query database
            db.query(sqlquery, newrecord, (err, result) => {
              if (err) {
                return console.error(err.message);
              }
              else
+             //sends data for th elist of books
              res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price);
              });
        });    
-
-    //    app.get('/bargainbooks', function(req, res) {
-    //     let sqlquery = "SELECT * FROM books WHERE price < 20";
-    //     db.query(sqlquery, (err, result) => {
-    //       if (err) {
-    //          res.redirect('./');
-    //       }
-    //       let newData = Object.assign({}, shopData, {availableBooks:result});
-    //       console.log(newData)
-    //       res.render('bargains.ejs', newData)
-    //     });
-    // });
-    
+   
     app.get('/bargainbooks', function(req, res) {
         // If the user is loggedin
         if (req.session.loggedin) {
-            console.log("here now") // Output username
             let sqlquery = "SELECT * FROM books WHERE price < 20";
             // execute sql query
-           
             db.query(sqlquery, (err, result) => {
                 if (err) {
                     res.redirect('./');
@@ -257,14 +235,8 @@ module.exports = function(app, shopData) {
         res.end();
     });
     
-    // app.get('/deleteuser', function (req, res) {
-    //     res.render('deleteuser.ejs', shopData);
-    //  });
-
      app.get('/deleteuser', function (req, res) {
         if (req.session.loggedin){
-
-        
             res.render('deleteuser.ejs', shopData);
         }else{
             res.send("Please log in");
@@ -279,12 +251,5 @@ module.exports = function(app, shopData) {
           console.log(data.affectedRows + " record(s) updated");
         });
         res.render('deleteuser', shopData);
-        
-        
-      });
-
-
-
-
-
+    });
 }
